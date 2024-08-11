2cbb0d7b4f9588168ac97f713687ef879a671ea0
GabrielGym <biel.ferreira12342@gmail.com>
2024-08-11 01:29:49 -0300

    feat: added axios and styled-components technology, created the basics of global styles, next step is to create a header.

--- Teste Front-end SERI.E/project_marvel/src/main.tsx
+++ Teste Front-end SERI.E/project_marvel/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import GlobalStyles from "./styles/GlobalStyles"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <GlobalStyles/>
+      <App />
+  </StrictMode>
 )
---
+++ Teste Front-end SERI.E/project_marvel/src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    :root{
+        --color-red: #FF1510;
+        --color-light_pink: #FDECEC;
+        --color-light_blue: #E7F6E7;
+        --color-gray1: #404040;
+        --color-gray2: #8C8C8C;
+        --color-gray3: #B9B9B9;
+    }
+    body {
+        font-family: "Work Sans", sans-serif;
+    }
+`;
